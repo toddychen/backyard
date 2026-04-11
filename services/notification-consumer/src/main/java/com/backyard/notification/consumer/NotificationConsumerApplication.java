f30d4e4ebc8b2fa85d368c62b3a9dbe07ede976d
Yi Chen <toddy.chen1@gmail.com>
2026-04-10 19:15:33 -0700

    @EnableKafka in NotificationConsumerApplication

--- services/notification-consumer/src/main/java/com/backyard/notification/consumer/NotificationConsumerApplication.java
+++ services/notification-consumer/src/main/java/com/backyard/notification/consumer/NotificationConsumerApplication.java
@@ -3,10 +3,12 @@ package com.backyard.notification.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.annotation.EnableKafka;
 
 import com.backyard.notification.common.kafka.KafkaTopicConfig;
 
 @SpringBootApplication
+@EnableKafka
 @Import(KafkaTopicConfig.class)
 public class NotificationConsumerApplication {
 
