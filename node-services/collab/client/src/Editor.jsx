4c20114e7b1d1d6e3946098a29c4e0d67f1f1b5a
Yi Chen <toddy.chen1@gmail.com>
2026-04-17 22:34:33 -0700

    Fix collab editor

--- node-services/collab/client/src/Editor.jsx
+++ node-services/collab/client/src/Editor.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
